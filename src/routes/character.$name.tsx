c9fde529c9994748645bc645c69f817307642be0
Husson <axel.husson@smile.fr>
2025-05-20 17:22:46 +0200

    Implement character detail retrieval in dynamic route
    - Added logic to fetch character data based on the route parameter.
    - Included a fallback message for when a character is not found.
    - Enhanced the RouteComponent to display character-specific information.

--- src/routes/character.$name.tsx
+++ src/routes/character.$name.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
-
+import { characters } from '@/data/characters'
+import { useParams } from '@tanstack/react-router'
 export const Route = createFileRoute('/character/$name')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const { name } = useParams({ from: '/character/$name' })
+  const character = characters.find(c => c.name === name)
+  console.log(character)
+  if (!character) {
+    return <div>Character not found</div>
+  }
   return <div>Hello "/character/character/$name"!</div>
 }
