f558343182fe366237a902bf97f81ac9695bd8ca
axel husson <aaaxel.hsson@gmail.com>
2025-06-25 23:46:35 +0200

    Refactor CharacterBiography component for improved readability and structure
    - Updated TypeScript interface for better clarity with semicolons.
    - Reformatted the JSX structure for consistent indentation and spacing.
    - Enhanced the layout of character biography, stats, and skills sections for better visual organization.

--- src/components/CharacterBiography.tsx
+++ src/components/CharacterBiography.tsx
@@ -1,48 +1,57 @@
 interface CharacterBiographyProps {
-  characterName: string
-  characterPortrait: string
-  characterBiography: string
-  characterWeapon?: string
+  characterName: string;
+  characterPortrait: string;
+  characterBiography: string;
+  characterWeapon?: string;
   characterStats?: {
-    health: number
-    attackPower: number
-    speed: number
-    defense: number
-    criticalRate: number
-  }
-  characterStartingSkills?: string[]
+    health: number;
+    attackPower: number;
+    speed: number;
+    defense: number;
+    criticalRate: number;
+  };
+  characterStartingSkills?: string[];
 }
 
-export default function CharacterBiography({characterName, characterPortrait, characterBiography, characterWeapon, characterStats, characterStartingSkills}: CharacterBiographyProps) {
+export default function CharacterBiography({
+  characterName,
+  characterPortrait,
+  characterBiography,
+  characterStats,
+  characterStartingSkills,
+}: CharacterBiographyProps) {
   return (
     <article className="flex flex-row gap-16 text-white/80 w-3/4 m-auto my-32">
-      <img src={characterPortrait} alt={characterName} className="w-2/5 h-auto" />
+      <img
+        src={characterPortrait}
+        alt={characterName}
+        className="w-2/5 h-auto"
+      />
       <div>
         <div>
           <h2 className="text-6xl font-trajan">{characterName}</h2>
-      </div>
-      <hr className="opacity-25 my-4" />
-            <div>
-        <h3 className="text-2xl font-trajan pb-4" >Biography</h3>
-        <p>{characterBiography}</p>
-      </div>
+        </div>
         <hr className="opacity-25 my-4" />
-      <div>
-        <h3 className="text-2xl font-trajan pb-4" >Base Stats</h3>
-        {Object.entries(characterStats ?? {}).map(([name, value]) => (
-          <div key={name} className="grid grid-cols-2 mb-2">
-            <p>{name}</p>
-            <p>{value}</p>
-          </div>
-        ))}
-      </div>
-      <hr className="opacity-25 my-4" />
-      <div>
-        <h3 className="text-2xl font-trajan pb-4" >Starting Skills</h3>
-        <p>{characterStartingSkills?.join(", ")}</p>
-      </div>
-
+        <div>
+          <h3 className="text-2xl font-trajan pb-4">Biography</h3>
+          <p>{characterBiography}</p>
+        </div>
+        <hr className="opacity-25 my-4" />
+        <div>
+          <h3 className="text-2xl font-trajan pb-4">Base Stats</h3>
+          {Object.entries(characterStats ?? {}).map(([name, value]) => (
+            <div key={name} className="grid grid-cols-2 mb-2">
+              <p>{name}</p>
+              <p>{value}</p>
+            </div>
+          ))}
+        </div>
+        <hr className="opacity-25 my-4" />
+        <div>
+          <h3 className="text-2xl font-trajan pb-4">Starting Skills</h3>
+          <p>{characterStartingSkills?.join(", ")}</p>
+        </div>
       </div>
     </article>
-  )
+  );
 }
